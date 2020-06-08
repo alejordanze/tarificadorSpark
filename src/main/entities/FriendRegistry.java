19517441c1f7db44e57b024d28e71136858a14ca
IvySaskia <saskiaivy@outlook.com>
2020-06-07 21:58:49 -0400

    Added FriendRegistry functions

--- src/main/entities/FriendRegistry.java
+++ src/main/entities/FriendRegistry.java
@@ -1,5 +1,6 @@
 package main.entities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,7 +21,41 @@ public class FriendRegistry {
     	friendList.put(ownerPhoneNumber, friendsPhoneNumbers);
     }
 
+    public void addAFriend(long ownerPhoneNumber, long friendPhoneNumbers) {
+    	friendList.get(ownerPhoneNumber).add(friendPhoneNumbers);
+    } 
+
     public List<Long> getFriends(long ownerPhoneNumber) {
     	return friendList.get(ownerPhoneNumber) ;
     }
+    
+	public void removeFriend(long ownerPhoneNumber, long friendPhoneNumber) {
+		friendList.get(ownerPhoneNumber).remove(friendPhoneNumber);
+	}
+	
+	public boolean isNumberFriend(long ownerPhoneNumber, long friendPhoneNumber) {
+		return friendList.get(ownerPhoneNumber).contains(friendPhoneNumber);
+	}
+	
+	public String getStringFriends(long ownerPhoneNumber){
+		String result = "";
+		List<Long> friends= getFriends(ownerPhoneNumber);
+		for(int i = 0; i < friends.size(); i++) {
+			result += (friends.get(i).toString()) + (i == friends.size() -1 ? "" : ",");
+		}
+		return result;
+	}
+	
+	public List<Long> getFriendsFromString(String friends) {
+		List<Long> friendsList = new ArrayList<Long>();
+		if(friends.length() > 0) {
+			friends = friends.substring(1, friends.length() - 1);
+			String[] list = friends.split(",");
+		    for(String text:list) {
+		    	friendsList.add(Long.valueOf(text));
+		     }
+		}
+		return friendsList;
+	}
 }
+
